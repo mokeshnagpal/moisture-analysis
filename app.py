655b553502efcf3d55e2aeb58edf8b529ddcd792
Mokesh Nagpal <98022916+mokeshnagpal@users.noreply.github.com>
2024-02-14 19:04:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ model = joblib.load('model.pkl')
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    return jsonify(data)
-    features = [
-        data['pH'], data['Soil EC'], data['Phosphorus'],
-        data['Potassium'], data['Urea'], data['T.S.P'],
-        data['M.O.P'], data['Temperature'], data['Plant Type']
-    ]
-
-    prediction = model.predict(np.array(features).reshape(1, -1))
+    prediction = model.predict(np.array(data['pH'], data['Soil EC'], data['Phosphorus'], data['Potassium'], data['Urea'], data['T.S.P'], data['M.O.P'], data['Temperature'], data['Plant Type']).reshape(1, -1))
 
     return jsonify({'moisture_prediction': prediction.tolist()})
 
