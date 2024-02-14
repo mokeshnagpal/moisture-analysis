f49edfde971db2d11f803abb3bf8de1724520f1f
Mokesh Nagpal <98022916+mokeshnagpal@users.noreply.github.com>
2024-02-14 19:24:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request, jsonify
-import numpy as np
-import joblib
+from numpy import array
+from joblib import load
+from json import loads
 
 app = Flask(__name__)
-model = joblib.load('model.pkl')
+model = load('model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    prediction = model.predict(np.array(data['pH'], data['Soil EC'], data['Phosphorus'], data['Potassium'], data['Urea'], data['T.S.P'], data['M.O.P'], data['Temperature'], data['Plant Type']).reshape(1, -1))
+    data = loads(request.get_json())
+    features = [
+        data['pH'], data['Soil EC'], data['Phosphorus'],
+        data['Potassium'], data['Urea'], data['T.S.P'],
+        data['M.O.P'], data['Temperature'], data['Plant Type']
+    ]
+
+    prediction = model.predict(array(features).reshape(1, -1))
 
     return jsonify({'moisture_prediction': prediction.tolist()})
 
