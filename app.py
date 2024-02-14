3cd68028cd4013f33b96f93097e766bcb0d1e15d
Mokesh Nagpal <98022916+mokeshnagpal@users.noreply.github.com>
2024-02-14 18:49:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = joblib.load('model.pkl')
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-
+    return jsonify(data)
     features = [
         data['pH'], data['Soil EC'], data['Phosphorus'],
         data['Potassium'], data['Urea'], data['T.S.P'],
